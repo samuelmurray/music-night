4f594f597fe50f700ba14e9ddf64c2392a4aae2d
Samuel Murray <samuelmu@kth.se>
2019-04-18 13:46:00 +0200

    Initial commit: InstrumentType

---
+++ src/InstrumentType.java
@@ -0,0 +1,35 @@
+public class InstrumentType {
+    private final String type;
+
+    private InstrumentType(String type) {
+        this.type = type;
+    }
+
+    public static InstrumentType banjo() {
+        return new InstrumentType("Banjo");
+    }
+
+    public static InstrumentType drums() {
+        return new InstrumentType("Drums");
+    }
+
+    public static InstrumentType guitar() {
+        return new InstrumentType("Guitar");
+    }
+
+    public static InstrumentType piano() {
+        return new InstrumentType("Piano");
+    }
+
+    public static InstrumentType synthesizer() {
+        return new InstrumentType("Synthesizer");
+    }
+
+    public static InstrumentType ukulele() {
+        return new InstrumentType("Ukulele");
+    }
+
+    public String getType() {
+        return type;
+    }
+}
