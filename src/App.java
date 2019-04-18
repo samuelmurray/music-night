231eaf317eec493d28009bbd3f0b372e23ba276e
Samuel Murray <samuelmu@kth.se>
2019-04-18 11:09:16 +0200

    Create and print band using existing musician

--- src/App.java
+++ src/App.java
@@ -1,7 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
         MusicalInstrument instrument = new MusicalInstrument("Synthesizer", "Suzuki");
         Musician musician = new Musician("Captor", instrument);
-        System.out.println(musician);
+        List<Musician> musicians = new ArrayList<>();
+        musicians.add(musician);
+        Band band = new Band("Frayrats", musicians);
+        System.out.println(band);
     }
 }
