ea67f32bf63127b133ab040f914495e01e367d86
Samuel Murray <samuelmu@kth.se>
2019-04-18 13:00:08 +0200

    Change to use BandFactory

--- src/App.java
+++ src/App.java
@@ -1,13 +1,10 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class App {
     public static void main(String[] args) {
-        MusicalInstrument instrument = new MusicalInstrument("Synthesizer", "Suzuki");
-        Musician musician = new Musician("Captor", instrument);
-        List<Musician> musicians = new ArrayList<>();
-        musicians.add(musician);
-        Band band = new Band("Frayrats", musicians);
-        System.out.println(band);
+        Band frayrats = BandFactory.createFrayrats();
+        System.out.println(frayrats);
+        frayrats.removeRandomMember();
+        System.out.println(frayrats);
+        frayrats.addMember(MusicianFactory.createCaptor());
+        System.out.println(frayrats);
     }
 }
